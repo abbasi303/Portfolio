ac1af9b8ad1ed4cc30bb8a6e477f588744ec6feb
abbasi303 <usalcgg18@gmail.com>
2024-02-11 23:57:09 +0100

    Deployment patch fix

---
+++ MyPortfolio/postcss.config.cjs
@@ -0,0 +1,8 @@
+// Correct content for postcss.config.cjs
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Include other necessary plugins here
+  ],
+}
